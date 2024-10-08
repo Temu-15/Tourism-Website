51607a887875329d50f7c26358df646ca258f1b6
Temu-15 <abuadanu89@gmail.com>
2024-10-08 23:05:53 +0300

    updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Cards({ title, location, travellers }) {
+function Cards({ title, location, travellers, image }) {
   const [liked, setLiked] = useState(false);
   const navigate = useNavigate();
   const handleClick = () => {
